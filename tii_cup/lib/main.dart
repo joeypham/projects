ab6ba1abebaa72b8466ce5d25f5cfdc986e7648d
Joey <phamluu234@gmail.com>
2022-08-13 00:02:38 -0500

    Newest Update

--- tii_cup/lib/main.dart
+++ tii_cup/lib/main.dart
@@ -14,8 +14,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://scontent-msp1-1.xx.fbcdn.net/v/t1.6435-9/142699515_3610419362358767_7155812626320225818_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=a26aad&_nc_ohc=oAbp_I2Rjx4AX9qsGCN&_nc_ht=scontent-msp1-1.xx&oh=00_AT8Pae7xptlR8dWCAj67dLwj9AOa1cFBQVlmQpBUnMpKhQ&oe=6318E666'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
